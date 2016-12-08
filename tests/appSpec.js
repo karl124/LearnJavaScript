8e4ec19358cc543a57426eecb6beac49a8f95b12
Karl <karl_singh@live.co.uk>
2016-12-08 11:51:59 +0000

    Commit work

--- tests/appSpec.js
+++ tests/appSpec.js
@@ -1,3 +1,16 @@
 /**
  * Created by davem on 27/11/2016.
  */
+
+decribe('LearnJavascript'), function(){
+    it('can show question view',function(){
+        learnjavascript.showView('#question-1);
+            expect($('view.-container .question-view').length).toEqual(1);
+    });
+
+    it ('shows the landing page view when their is not hash',removeFunctionWithId()
+    {
+        learnjavascript.showView('');
+        expect($('.view-container .landing-view').length).toEqual(1);
+    });
+});
